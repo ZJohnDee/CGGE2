83d78f9b3651d8f8285058239b6572d3e6b8597c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-17 15:14:10 +0200

    Restruct of core

--- src/main/java/de/cg/cgge/ctrl/CGGE2.java
+++ src/main/java/de/cg/cgge/ctrl/CGGE2.java
@@ -2,16 +2,17 @@ package de.cg.cgge.ctrl;
 
 import de.cg.cgge.game.GameInstance;
 
-public class App {
+public class CGGE2 {
 
-    public static String release = "pre-1.0-b7";
+    private static String version = "pre-1.0-b7";
+    private static int release = 0;
 
     public static boolean package_uiplus = false;
 
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("CGGE2 Game Engine");
-            System.out.println("Current Version: " + release);
+            System.out.println("Current Version: " + version);
         }
 
         if (args.length == 1) {
@@ -24,5 +25,13 @@ public class App {
                 System.out.println("UIPlus: " + package_uiplus);
             }
         }
+    }
+
+    public static String getVersion() {
+        return version;
+    }
+
+    public static int getRelease() {
+        return release;
     }
 }
