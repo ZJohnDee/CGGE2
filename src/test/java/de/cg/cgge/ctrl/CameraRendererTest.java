3868ded021145c5aa07f1fa3099bc1409cd112d1
BuildTools <unconfigured@null.spigotmc.org>
2020-05-10 15:10:30 +0200

    CameraRendererTest -> Added a test with zooming

--- src/test/java/de/cg/cgge/ctrl/CameraRendererTest.java
+++ src/test/java/de/cg/cgge/ctrl/CameraRendererTest.java
@@ -21,7 +21,23 @@ public class CameraRendererTest {
 
         boolean test = cam.isInView(game.getWidth()-25, game.getHeight()-25, 50, 50);
 
-        Assert.assertEquals(test, true);
+        Assert.assertEquals(true, test);
     }
+
+    @Test
+    public void testInViewZoom() {
+        Camera cam = game.getRoom().getCamera();
+        cam.setZoom(2.0f);
+
+        int x = game.getWidth()/2-25;
+        int y = game.getHeight()/2-25;
+        int w = 50;
+        int h = 50;
+
+        boolean test = cam.isInView(x,y,w,h);
+
+        Assert.assertEquals(true, test);
+    }
+
 
 }
